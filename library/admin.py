75e32c0dcd12d3cb90fda2d4ced53a8d60c26b5c
Abraham <abrahamperezquiroz0@gmail.com>
2024-02-16 15:11:48 -0600

    01 App librari module and questions models

---
+++ library/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Question, Module
+# Register your models here.
+admin.site.register(Module)
+admin.site.register(Question)
---
+++ library/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Module(models.Model):
+    name = models.CharField(verbose_name = 'Module names', max_length = 30)
+    description = models.CharField(verbose_name = 'Modules description', max_length = 123456789)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'module'
+        verbose_name_plural = 'modulos'
+    
+
+class Question(models.Model):
+    module = models.ForeignKey(Module, on_delete = models.CASCADE)
+    question_text = models.CharField( null = True, blank = True, verbose_name = 'La preguta...LA PREGUNTA :O', max_length = 52)
+    question_image = models.ImageField(verbose_name = 'Imagen de la pregunta', upload_to = 'questions')
+    answer1 = models.CharField(verbose_name = '1', max_length = 200)
+    answer2 = models.CharField(verbose_name = '2', max_length = 200)
+    answer3 = models.CharField(null = True, blank = True, verbose_name = '3', max_length = 200)
+    answer4 = models.CharField(null = True, blank = True, verbose_name = '4', max_length = 200)
+    correct = models.CharField(verbose_name = 'La respuesta siiii uwu', max_length = 8)
+
+    def __str__(self):
+        return f"{ self.module } Pregunta { self.id}"
+    
+    class Meta:
+        verbose_name = 'pregunta'
+        verbose_name_plural = 'preguntas'
