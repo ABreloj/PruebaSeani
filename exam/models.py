c561fde46fa28a344a085f95a6d39e411c63e662
Abraham <abrahamperezquiroz0@gmail.com>
2024-03-07 10:56:19 -0600

    05 Exam, ExamModule and Breakdown models

--- exam/models.py
+++ exam/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from career.models import Career
+from library.models import Module, Question
+
 # Create your models here.
 class Stage(models.Model):
     stage = models.IntegerField(
@@ -13,9 +17,12 @@ class Stage(models.Model):
     def month(self):
         months = ['Enero', 'Febrero', 'Marzo', 'April', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octrube', 'Noviembre', 'Diciembre']
         return months[self.application_date.month - 1]
+    month.short_description = 'Mes'
 
     def year(self):
         return self.application_date.year
+    year.short_description = 'Año'
+
 
     def __str__(self):
         return f"{ self.stage } - { self.month() } { self.year() }"
@@ -24,6 +31,39 @@ class Stage(models.Model):
         verbose_name = "etapa"
         verbose_name_plural = "etapas"    
 
+class Exam(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Usuario")
+    career = models.ForeignKey(Career, on_delete=models.CASCADE, verbose_name="Carrera")
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE, verbose_name="Etapa")
+    modules = models.ManyToManyField(Module, through='ExamModule', verbose_name="Modulo")
+    questions = models.ManyToManyField(Question, through='Breakdown', verbose_name="Preguntas")
+    score = models.FloatField(default=0.0, verbose_name="Calificacion")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualizacion")
+
+    def __str__(self):
+        return f"{ self.user } - { self.career } - { self.score }"
+
+    class Meta:
+        verbose_name = "examen"
+        verbose_name_plural = "examenes"
+
+class ExamModule(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE, verbose_name="Examen")
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, verbose_name="Module")
+    activate = models.BooleanField(default=True, verbose_name="Activado")
+    score = models.FloatField(default=0.0, verbose_name="Calificacion")   
+
+    def __str__(self):
+        return f"{ self.module } - { self.score }"
 
 
-        
+class Breakdown(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE, verbose_name="Examen")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Preguntas")
+    answer = models.CharField(max_length=11, default = '-', verbose_name="Respuestas")
+    correct = models.CharField(max_length=11, default = '-', verbose_name="Respuesta correcta") 
+
+    def __str__(self):
+        return f"{ self.question } - { self.answer }"
+    
